f43414d406dd9de9f51ba756472ba308b536f021
brian <brian@microcomaustralia.com.au>
2011-06-29 08:29:07 +0000

    More work for mobile devices.

--- media/js/mouse.js
+++ media/js/mouse.js
@@ -1,18 +1,41 @@
-var _startX = 0;            // mouse starting positions
-var _startY = 0;
-var _offsetX = 0;           // current element offset
-var _offsetY = 0;
-var _dragElement;
-var _fixedElement;
-var _goto = null;
-
 InitDragDrop();
 
-var next_image = new Image()
-next_image.src = next_photo_thumb_url
+if (next_photo_thumb_url != "None") {
+    var next_image = new Image()
+    next_image.src = next_photo_thumb_url
+}
 
-var prev_image = new Image()
-prev_image.src = prev_photo_thumb_url
+if (prev_photo_thumb_url != "None") {
+    var prev_image = new Image()
+    prev_image.src = prev_photo_thumb_url
+}
+
+function resize_photo() {
+    var img = document.getElementById("photo")
+    var width = img.naturalWidth
+    var height = img.naturalHeight
+    var aspect = width/height
+
+    if (width > window.innerWidth) {
+        width = window.innerWidth
+        height = width / aspect
+    }
+
+    if (height > window.innerHeight) {
+        height = window.innerHeight
+        width = height * aspect
+    }
+
+    if (window.innerWidth <= 700) {
+        img.style.marginLeft = ((window.innerWidth-width)/2) + "px"
+    }
+
+    img.width = width
+    img.height = height
+}
+
+window.onload = function(e) { resize_photo(); }
+window.onresize = function(e) { resize_photo() }
 
 function getElementsByClassName(className)
 {
@@ -32,8 +55,8 @@ function getElementsByClassName(className)
 
 function InitDragDrop()
 {
-    document.onmousedown = OnMouseDown;
-    document.onmouseup = OnMouseUp;
+    document.onmousedown = OnDown;
+    document.ontouchstart = OnDown;
 }
 
 function findPos(obj) {
@@ -47,107 +70,223 @@ function findPos(obj) {
     return [curleft,curtop];
 }
 
-function OnMouseDown(e)
+var _loading = false;
+var _ignore = false;
+function OnDown(e)
 {
     // IE is retarded and doesn't pass the event object
-    if (e == null) 
-        e = window.event; 
+    if (e == null) e = window.event;
+
+    var _startX = 0;            // mouse starting positions
+    var _startY = 0;
+    var _scrollX = 0;
+    var _scrollY = 0;
+    var _offsetX = 0;           // current element offset
+    var _offsetY = 0;
+    var _dragElement;
+    var _fixedElement;
+    var _goto = null;
+    var _move = false;
+
+    var OnMove = function(e)
+    {
+        // IE is retarded and doesn't pass the event object
+        if (e == null) e = window.event;
+        if( event.targetTouches )
+        {
+            X = event.targetTouches[0].pageX;
+            Y = event.targetTouches[0].pageY;
+
+        }
+        else {
+            X = event.pageX || event.x;
+            Y = event.pageY || event.y;
+        }
+        _move = true;
+
+//        console.log(_scrollX+","+_scrollY+","+Y+","+_startY)
+//        window.scrollTo(_scrollX,_scrollY-(Y-_startY))
+
+        if (_dragElement != null)
+        {
+            var visibility = "visible";
+
+            if (e == null) 
+                var e = window.event;
+
+            // this is the actual "drag code"
+            _dragElement.style.left = (_offsetX + X - _startX) + 'px';
+            _dragElement.style.top = (_offsetY + Y - _startY) + 'px';
+
+            var movedX = (X - _startX);
+            if (movedX > 20 && prev_photo_url!="None") {
+                _fixedElement.src = prev_photo_thumb_url
+                 _fixedElement.onload = resize_photo
+                _goto = "prev"
+                visibility = "hidden";
+            } else if (movedX < -20 && next_photo_url!="None") {
+                _fixedElement.src = next_photo_thumb_url
+                _fixedElement.onload = resize_photo
+                _goto = "next"
+                visibility = "hidden";
+            } else {
+                _fixedElement.src = this_photo_thumb_url
+                _fixedElement.onload = resize_photo
+                _goto = null
+            }
+
+            var list = getElementsByClassName("photo_detail_block")
+            list = list.concat(getElementsByClassName("photo_detail_summary"))
+            for (x in list) {
+                list[x].style.visibility = visibility
+            }
+        }
+    }
+
+    var OnUp = function(e)
+    {
+        // IE is retarded and doesn't pass the event object
+        if (e == null) e = window.event;
+
+        if (!_move) {
+            _ignore = true
+        }
+
+        if (_dragElement != null)
+        {
+            // we're done with these events until the next OnDown
+            document.onmousemove = null;
+            document.ontouchmove = null;
+            document.onselectstart = null;
+            document.onmouseup = null;
+            document.ontouchend = null;
+            document.ontouchcancel = null;
+            _dragElement.ondragstart = null;
+
+            document.getElementById("content").removeChild(_dragElement);
+
+            // this is how we know we're not dragging      
+            _dragElement = null;
+
+            if (_goto == "prev") {
+                _loading = true
+                parent.location = prev_photo_url;
+            } else if (_goto == "next") {
+                _loading = true
+                parent.location = next_photo_url;
+            }
+        }
+    }
+
+    var OnCancel = function(e)
+    {
+        // IE is retarded and doesn't pass the event object
+        if (e == null) e = window.event;
+
+        if (_dragElement != null)
+        {
+            // we're done with these events until the next OnDown
+            document.onmousemove = null;
+            document.ontouchmove = null;
+            document.onselectstart = null;
+            document.onmouseup = null;
+            document.ontouchend = null;
+            document.ontouchcancel = null;
+            _dragElement.ondragstart = null;
+
+            document.getElementById("content").removeChild(_dragElement);
+
+            // this is how we know we're not dragging      
+            _dragElement = null;
+
+            // Reset page back the way it was
+            _fixedElement.src = this_photo_thumb_url
+            var list = getElementsByClassName("photo_detail_block")
+            list = list.concat(getElementsByClassName("photo_detail_summary"))
+            for (x in list) {
+                list[x].style.visibility = "visible"
+            }
+        }
+    }
+
+    if( event.targetTouches )
+    {
+        X = event.targetTouches[0].pageX;
+        Y = event.targetTouches[0].pageY;
+
+    }
+    else {
+        X = event.pageX || event.x;
+        Y = event.pageY || event.y;
+    }
 
     // IE uses srcElement, others use target
     var target = e.target != null ? e.target : e.srcElement;
 
     // for IE, left click == 1
     // for Firefox, left click == 0
-    if (target.id == "photo" && (e.button == 1 && window.event != null || 
-        e.button == 0))
+    if (target.id != "photo") {
+        return true;
+    }
+
+    if (_loading) {
+        return true;
+    }
+
+    if (_ignore) {
+        _ignore = false;
+        return true;
+    }
+
+    if (e.button == 1 && window.event != null || e.button == 0 || e.type == "touchstart")
     {
+//        e.preventDefault();
+//        e.stopPropagation();
+
         // grab the mouse position
-        _startX = e.clientX;
-        _startY = e.clientY;
+        _startX = X
+        _startY = Y
+
+        // grab the scroll position
+        _scrollX = window.scrollX
+        _scrollY = window.scrollY
 
         // grab the clicked element's position
         var pos = findPos(target)
         _offsetX = pos[0]
         _offsetY = pos[1]
 
-        // we need to access the element in OnMouseMove
+        // we need to access the element in OnMove
         _fixedElement = target;
-        _dragElement = document.createElement('img');
+        _dragElement = document.createElement('img')
         _dragElement.src = _fixedElement.src
-        _dragElement.style.position = "absolute";
-        _dragElement.style.left = (_offsetX + e.clientX - _startX) + 'px';
-        _dragElement.style.top = (_offsetY + e.clientY - _startY) + 'px';
+        _dragElement.style.width = _fixedElement.offsetWidth + 'px'
+        _dragElement.style.position = "absolute"
+        _dragElement.style.left = (_offsetX + X - _startX) + 'px'
+        _dragElement.style.top = (_offsetY + Y - _startY) + 'px'
+
         document.getElementById("content").appendChild(_dragElement);
 
         // bring the clicked element to the front while it is being dragged
-        _dragElement.style.zIndex = 10000;
+        _dragElement.style.zIndex = 10000
 
         // tell our code to start moving the element with the mouse
-        document.onmousemove = OnMouseMove;
+        document.onmousemove = OnMove
+        document.ontouchmove = OnMove
+        document.onmouseup = OnUp
+        document.ontouchend = OnUp
+        document.ontouchcancel = OnCancel
 
         // cancel out any text selections
-        document.body.focus();
+        document.body.focus()
 
         // prevent text selection in IE
-        document.onselectstart = function () { return false; };
+        document.onselectstart = function () { return false; }
         // prevent IE from trying to drag an image
-        target.ondragstart = function() { return false; };
+        target.ondragstart = function() { return false; }
 
         // prevent text selection (except IE)
-        return false;
+        return false
     }
 }
 
-function OnMouseMove(e)
-{
-    var visibility = "visible";
-
-    if (e == null) 
-        var e = window.event;
-
-    // this is the actual "drag code"
-    _dragElement.style.left = (_offsetX + e.clientX - _startX) + 'px';
-    _dragElement.style.top = (_offsetY + e.clientY - _startY) + 'px';
-
-    var movedX = (e.clientX - _startX);
-    if (movedX > 20 && prev_photo_url!="None") {
-        _fixedElement.src = prev_photo_thumb_url
-        _goto = "prev"
-        visibility = "hidden";
-    } else if (movedX < -20 && next_photo_url!="None") {
-        _fixedElement.src = next_photo_thumb_url
-        _goto = "next"
-        visibility = "hidden";
-    } else {
-        _fixedElement.src = _dragElement.src
-        _goto = null
-    }
-
-    var list = getElementsByClassName("photo_detail_block")
-    list = list.concat(getElementsByClassName("photo_detail_summary"))
-    for (x in list) {
-        list[x].style.visibility = visibility
-    }
-}
-
-function OnMouseUp(e)
-{
-    if (_dragElement != null)
-    {
-        // we're done with these events until the next OnMouseDown
-        document.onmousemove = null;
-        document.onselectstart = null;
-        _dragElement.ondragstart = null;
-
-        document.getElementById("content").removeChild(_dragElement);
-
-        // this is how we know we're not dragging      
-        _dragElement = null;
-
-        if (_goto == "prev") {
-            parent.location = prev_photo_url;
-        } else if (_goto == "next") {
-            parent.location = next_photo_url;
-        }
-    }
-}
