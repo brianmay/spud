f475f4543b79a5928231fa4ff019cf098f28222d
Brian May <brian@microcomaustralia.com.au>
2012-09-14 16:05:23 +1000

    Update settings for Django 1.4 compatability.

--- spud/settings.py
+++ spud/settings.py
@@ -30,20 +30,14 @@ STATIC_ROOT = "/usr/share/spud/media"
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 STATIC_URL = '/spud/media/'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/spud/media/admin/'
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
