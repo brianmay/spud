5bcbc7bef295b7f31eef88c0a5a7fd93bfcd30c9
Brian May <brian@microcomaustralia.com.au>
2013-03-28 11:13:41 +1100

    Replace django.conf.urls.defaults with django.conf.urls for Django 1.4

--- spud/urls.py
+++ spud/urls.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls.defaults import url, include, patterns
+from django.conf.urls import url, include, patterns
 
 from django.contrib import admin
 admin.autodiscover()
