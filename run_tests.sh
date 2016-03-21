59ab332888c6734a4b6357b2c01743878306824f
Brian May <brian@linuxpenguins.xyz>
2016-03-21 16:25:54 +1100

    Fix up problems running tests

--- run_tests.sh
+++ run_tests.sh
@@ -14,35 +14,54 @@ echo ""
 echo "FLAKE8"
 echo "############################"
 flake8 --ignore=E501 --filename="south_migrations" spud
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
+
 flake8 --ignore=E501 --filename="migrations" spud
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
+
 flake8 --exclude="south_migrations,migrations" spud
 if [ ! $? -eq 0 ]
 then
     RETURN=1
 fi
-echo -e "\n\n"
 
 echo ""
 echo "STATIC FILES"
 echo "############################"
 ./manage.py collectstatic --settings=spud.tests.settings -v 2 --noinput
-
-echo ""
-echo "TESTS - Python 2"
-echo "############################"
-python2 ./manage.py test --settings=spud.tests.settings -v 2 $TESTS
 if [ ! $? -eq 0 ]
 then
     RETURN=1
 fi
 
-#echo ""
-#echo "TESTS - Python 3"
-#echo "############################"
-#python3 ./manage.py test --settings=spud.tests.settings -v 2 $TESTS
-#if [ ! $? -eq 0 ]
-#then
-#    RETURN=1
-#fi
+tsc spud/static/js/*.ts
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
+
+echo ""
+echo "TESTS - Python 2"
+echo "############################"
+python2 -m pytest
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
+
+echo ""
+echo "TESTS - Python 3"
+echo "############################"
+python3 -m pytest
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
 
 exit $RETURN
