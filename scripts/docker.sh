ef1e614a7a09417032820e403cc760f1d0965249
Brian May <brian@linuxpenguins.xyz>
2017-09-02 09:01:27 +1000

    Run collectstatic as root

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,9 +4,7 @@ set -e
 if ! test -d /var/lib/spud/static
 then
     mkdir /var/lib/spud/static
-    chown www-data:root -R /var/lib/spud/static
-    sudo -u www-data python3 manage.py collectstatic --noinput
-    chown root:root -R /var/lib/spud/static
+    python3 manage.py collectstatic --noinput
 fi
 
 sudo -u www-data ./scripts/start.sh
