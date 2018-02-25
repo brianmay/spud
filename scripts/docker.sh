e4820186616dc4fbbc1d85751e02a27ed81b4f3f
Brian May <brian@linuxpenguins.xyz>
2018-02-25 11:37:04 +1100

    Always run collectstatic even if directory exists

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -e
 
-if ! test -d /var/lib/spud/static
-then
-    mkdir /var/lib/spud/static
-    python3 manage.py collectstatic --noinput
-fi
-
+python3 manage.py collectstatic --noinput
 sudo -u www-data ./scripts/start.sh
