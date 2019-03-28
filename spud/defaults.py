ef9f3f5b497e0b1701b61336c5db3e48b5a34336
Brian May <brian@linuxpenguins.xyz>
2019-03-28 17:21:56 +1100

    Remove unused imports

--- spud/defaults.py
+++ spud/defaults.py
@@ -16,10 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import, unicode_literals
 
-import sys
 from socket import getfqdn
-
-import django
 
 
 ###
