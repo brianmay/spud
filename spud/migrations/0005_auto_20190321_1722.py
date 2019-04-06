1a2b608d517ea4a41f859dba42032aea9727d3e6
Brian May <brian@linuxpenguins.xyz>
2019-04-06 12:13:49 +1100

    Handle conflicting photos better

--- spud/migrations/0005_auto_20190321_1722.py
+++ spud/migrations/0005_auto_20190321_1722.py
@@ -16,73 +16,75 @@ def populate_photo_files(apps, schema_editor):
     photo_file = apps.get_model('spud', 'photo_file')
 
     photo = apps.get_model('spud', 'photo')
-    logger.info("Migrating photos.")
-    for pf in photo.objects.all():
-        dir = os.path.join("orig", pf.path)
-        full_path = os.path.join(settings.IMAGE_PATH, dir, pf.name)
+
+    for p in photo.objects.all():
+        dir = os.path.join("orig", p.path)
+        full_path = os.path.join(settings.IMAGE_PATH, dir, p.name)
         media = get_media(full_path)
         mime_type, _ = mimetypes.guess_type(full_path)
         sha256_hash = media.get_sha256_hash()
         xy_size = media.get_size()
         num_bytes = media.get_num_bytes()
+
+        print(f"{p.pk} {p.full_path} {sha256_hash}")
+        if photo_file.objects.filter(sha256_hash).count > 0:
+            print("SKIPPING {pf.pk} {pf.full_path} {sha256_hash}")
+            continue
+
         photo_file.objects.create(
-            photo_id=pf.pk,
+            photo_id=p.pk,
             size_key="orig",
             width=xy_size[0],
             height=xy_size[1],
             mime_type=mime_type,
             dir=dir,
-            name=pf.name,
+            name=p.name,
             is_video=False,
             sha256_hash=sha256_hash,
             num_bytes=num_bytes,
         )
 
-    photo_thumb = apps.get_model('spud', 'photo_thumb')
-    logger.info("Migrating thumbs.")
-    for pf in photo_thumb.objects.all():
-        short_name, extension = os.path.splitext(pf.photo.name)
-        name = f"{short_name}.jpg"
-        dir = os.path.join("thumb", pf.size, pf.photo.path)
-        full_path = os.path.join(settings.IMAGE_PATH, dir, name)
-        media = get_media(full_path)
-        sha256_hash = media.get_sha256_hash()
-        num_bytes = media.get_num_bytes()
-        photo_file.objects.create(
-            photo_id=pf.photo_id,
-            size_key=pf.size,
-            width=pf.width,
-            height=pf.height,
-            mime_type="image/jpeg",
-            dir=dir,
-            name=name,
-            is_video=False,
-            sha256_hash=sha256_hash,
-            num_bytes=num_bytes,
-        )
+        for pf in p.photo_thumb_set.all():
+            short_name, extension = os.path.splitext(pf.photo.name)
+            name = f"{short_name}.jpg"
+            dir = os.path.join("thumb", pf.size, pf.photo.path)
+            full_path = os.path.join(settings.IMAGE_PATH, dir, name)
+            media = get_media(full_path)
+            sha256_hash = media.get_sha256_hash()
+            num_bytes = media.get_num_bytes()
+            photo_file.objects.create(
+                photo_id=pf.photo_id,
+                size_key=pf.size,
+                width=pf.width,
+                height=pf.height,
+                mime_type="image/jpeg",
+                dir=dir,
+                name=name,
+                is_video=False,
+                sha256_hash=sha256_hash,
+                num_bytes=num_bytes,
+            )
 
-    photo_video = apps.get_model('spud', 'photo_video')
-    logger.info("Migrating videos.")
-    for pf in photo_video.objects.all():
-        short_name, _ = os.path.splitext(pf.photo.name)
-        name = f"{short_name}.{pf.extension}"
-        dir = os.path.join("video", pf.size, pf.photo.path)
-        full_path = os.path.join(settings.IMAGE_PATH, dir, name)
-        media = get_media(full_path)
-        sha256_hash = media.get_sha256_hash()
-        num_bytes = media.get_num_bytes()
-        photo_file.objects.create(
-            photo_id=pf.photo_id,
-            size_key=pf.size,
-            width=pf.width,
-            height=pf.height,
-            mime_type=pf.format,
-            dir=dir,
-            name=name,
-            is_video=True,
-            sha256_hash=sha256_hash,
-            num_bytes=num_bytes,
-        )
+        for pf in p.photo_video_set.all():
+            short_name, _ = os.path.splitext(pf.photo.name)
+            name = f"{short_name}.{pf.extension}"
+            dir = os.path.join("video", pf.size, pf.photo.path)
+            full_path = os.path.join(settings.IMAGE_PATH, dir, name)
+            media = get_media(full_path)
+            sha256_hash = media.get_sha256_hash()
+            num_bytes = media.get_num_bytes()
+            photo_file.objects.create(
+                photo_id=pf.photo_id,
+                size_key=pf.size,
+                width=pf.width,
+                height=pf.height,
+                mime_type=pf.format,
+                dir=dir,
+                name=name,
+                is_video=True,
+                sha256_hash=sha256_hash,
+                num_bytes=num_bytes,
+            )
 
 
 class Migration(migrations.Migration):
